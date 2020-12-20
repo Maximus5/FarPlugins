b2460a718e01d9d068edfc318a6c7be56fbd882b
Maksim Moisiuk <maksim.moisiuk@avira.com>
2020-12-20 18:35:25 +0100

    Uninstall sources v1.10.9.0

--- Uninstall/src/vc.build.release.bat
+++ Uninstall/src/vc.build.release.bat
@@ -11,6 +11,8 @@ if exist "%VS90COMNTOOLS%..\..\VC\BIN\vcvars32.bat" (
 set WIDE=1
 set IA64=
 set AMD64=
+set CPU=
+set PLATFORM=
 set DEBUG=
 set RELEASE=1
 
@@ -18,14 +20,15 @@ nmake /A /B /F %MAKEFILE%
 rem nmake /F %MAKEFILE%
 if errorlevel 1 goto end
 
-rem rem call "%VS90COMNTOOLS%..\..\VC\BIN\vcvarsx86_amd64.bat"
-rem if exist "%VS90COMNTOOLS%..\..\VC\BIN\vcvarsx86_amd64.bat" (
-rem   call "%VS90COMNTOOLS%..\..\VC\BIN\vcvarsx86_amd64.bat"
-rem ) else (
-rem   call "C:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvarsx86_amd64.bat"
-rem )
-rem set AMD64=1
-rem nmake /A /B /F %MAKEFILE%
-rem rem nmake /F %MAKEFILE%
+if exist "%VS90COMNTOOLS%..\..\VC\BIN\vcvarsx86_amd64.bat" (
+  call "%VS90COMNTOOLS%..\..\VC\BIN\vcvarsx86_amd64.bat"
+) else (
+  call "C:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvarsx86_amd64.bat"
+)
+set AMD64=1
+set CPU=AMD64
+set PLATFORM=x64
+nmake /A /B /F %MAKEFILE%
+rem nmake /F %MAKEFILE%
 
 :end

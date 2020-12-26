ddeb96be32e547ea3a007794f87880dc9f31d66c
Maximus5 <ConEmu.Maximus5@gmail.com>
2020-12-26 20:23:53 +0100

    Add diz_c0 missed files

--- Col0/diz_c0/version.h
+++ Col0/diz_c0/version.h
@@ -1,7 +1,7 @@
 
 #define MVV_1 3
 #define MVV_2 1
-#define MVV_3 0
+#define MVV_3 1
 #define MVV_4 0
 #define MVV_4a ""
 
@@ -9,7 +9,7 @@
 #define STRING2(x) #x
 #define STRING(x) STRING2(x)
 
-#define DIZC0VERS STRING(MVV_1) "." STRING(MVV_2)
+#define DIZC0VERS STRING(MVV_1) "." STRING(MVV_2) "." STRING(MVV_3)
 #define DIZC0VERN MVV_1,MVV_2,MVV_3,MVV_4
 
 #ifdef _WIN64

3d5015557e148e67184457b99b7802bb1c9dd44f
Maximus5 <ConEmu.Maximus5@gmail.com>
2020-12-26 20:25:26 +0100

    ver_c0 build script

--- Col0/ver_c0/build_verc0.bat
+++ Col0/ver_c0/build_verc0.bat
@@ -2,14 +2,18 @@
 
 set MAKEFILE=makefile_verc0
 
+setlocal
 call "%~dp0..\..\tools\vc.build.set.x32.bat"
-
 set WIDE=1
 set IA64=
 set AMD64=
-
 nmake /A /B /F %MAKEFILE% FAR_UNICODE=4245
+endlocal
 
+setlocal
 call "%~dp0..\..\tools\vc.build.set.x64.bat"
+set WIDE=1
+set IA64=
 set AMD64=1
 nmake /A /B /F %MAKEFILE% FAR_UNICODE=4245
+endlocal
